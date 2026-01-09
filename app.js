3f433979ef4e1ecd95a05a61796e9c89e54374b4
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2026-01-09 11:45:51 +0000

    5章【jQueryでHTMLのCSS要素を操作しよう】

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(document).ready(function(){
-  $('body').html('<h1>Hello jQuery!!</h1>');
+//5章
+$(function (){
+  //確認問題
+  $('.box1').slideDown(2500, function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(2500);
+  });
+//  $('.box1').hide();
+
+//  $('.box1').show();
+//  $('.box1').css({'background-color': '#0000FF'});
+
+//  $('.box1').slideUp(2500);
+
+//  $('.box1').slideDown(2500);
+
+//  $('.box1').css({
+//    'background-color': '#0000FF',
+//    'height': '100px'
+//  });
 });
+
+
+
+
+
+
+
+//4章
+//$(document).ready(function(){
+//  $('body').html('<h1>Hello jQuery!!</h1>');
+//});
